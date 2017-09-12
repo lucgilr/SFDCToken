935f9a96345e9dee097d9bf433a6a129c2fbee4a
Gil Román <609081@cognizant.com>
2017-09-12 09:07:51 +0200

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function() {
+
+  "use strict";
+
+  console.log("document ready");
+
+  $('#loginInfo').submit(function(event) {
+
+  	console.log("username: "+$('#username').val());
+  	console.log("password: "+$('#password').val());
+
+  	var formData = 'grant_type=password'+
+  	 				'&client_id=3MVG98_Psg5cppyZFSE9IdHLIUHC7NS3_V8Oec0wsphdsCcAg5GtxeAciNbKunR5FKkI4pzmWgw5v5qrULPfv'+
+  	 				'&client_secret=2622055315156138549'+
+  	 				'&username='+$('#username').val()+
+  	 				'&password='+$('#password').val()
+
+	$.ajax({
+		type: 'POST',
+		url: 'https://login.salesforce.com/services/oauth2/token',
+		data: formData,
+		crossDomain: true,
+		headers: {
+      		'Content-Type': 'application/x-www-form-urlencoded'
+    	},
+		success : function() {
+			console.log("success!");
+			alert(result);
+	    },
+	    error : function(error) {
+	    	console.log("error...");
+			alert(error);
+	    }
+	});        
+	return false;
+  });
+
+});
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	try {		
+		res.render('index');
+	} catch (exception) {
+		console.log(exception);
+	}
+});
+
+router.post('/', function(req, res, next) {
+	try {		
+	    console.log('req received');
+	    console.log('Username: '+req.body.username);
+	    console.log('Password: '+req.body.password);
+	} catch (exception) {
+		console.log(exception);
+	}
+});
+
+module.exports = router;
