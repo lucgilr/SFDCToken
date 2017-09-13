573951771c6e3b61911e59f5341a7cadb3421cc5
Gil Román <609081@cognizant.com>
2017-09-13 09:41:12 +0200

    etag disabled

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.set('etag',false);
 app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.json());
