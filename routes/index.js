40ce8ee1f1b3b44945d4ced9313d3ae81cb0dc8a
David MG <dmgarcia2@gmail.com>
2017-09-15 12:04:19 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var app = express();
 
-var https = require('https');
+var request = require('request');
 var querystring = require('querystring');
 
 app.use(bodyParser.json());
@@ -44,8 +44,10 @@ router.get('/callback', function(req, res, next) {
 		var tokenResponse = null;
 		var communityUrl = 'https://vldtest-developer-edition.eu6.force.com';
 
+		/*
 		var postData = "code=" + code + "&grant_type=authorization_code&client_id=3MVG98_Psg5cppyZT.V54UWRRSi0tcHOtdsX0VQ3DfW.Rf479WhMV9nVCTIp39qeCQLIjZsWIL4HDWRFX6n_P" +
 					   "&client_secret=9219936345482605892&redirect_uri=https://sfdc-login-token.herokuapp.com/callback";
+		*/
 
 		var postOptions = {
 			url: communityUrl + '/embeddedlogin/services/oauth2/token',
@@ -53,92 +55,68 @@ router.get('/callback', function(req, res, next) {
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 				'Content-Length': Buffer.byteLength(postData)
+			},
+			form: {
+				code: code,
+				grant_type: 'authorization_code',
+				client_id: '3MVG98_Psg5cppyZT.V54UWRRSi0tcHOtdsX0VQ3DfW.Rf479WhMV9nVCTIp39qeCQLIjZsWIL4HDWRFX6n_P',
+				client_secret: '9219936345482605892',
+				redirect_uri: 'https://sfdc-login-token.herokuapp.com/callback'
 			}
 		};
 
-		console.log('CALLBACK - postData: ' + postData);
-		console.log('CALLBACK - postOptions: ' + JSON.stringify(postOptions));
+		console.log('CALLBACK - POST - options: ' + JSON.stringify(postOptions));
 
-		 var postReq = https.request(postOptions, function(postRes) {
-			console.log('CALLBACK - statusCode:', postRes.statusCode);
-			console.log('CALLBACK - headers:', postRes.headers);
+		request(postOptions, function(error, httpResponse, postResponse) {
+			console.log('CALLBACK - POST - error:', error);
+			console.log('CALLBACK - POST - statusCode:', httpResponse && httpResponse.statusCode);
+			console.log('CALLBACK - POST - headers:', httpResponse && httpResponse.headers);
+			console.log('CALLBACK - POST - response:', postResponse);
 
-			postRes.setEncoding('utf8');
+			if (httpResponse && httpResponse.statusCode != 200) {
+				res.status(httpResponse.statusCode).json(JSON.parse(postResponse));
+				return;
+			}
 
-			var response = '';
-			postRes.on('data', function (chunk) {
-				response += chunk;
-			});
+			var access_token = postResponse.access_token;
+			var identity = postResponse.id;
 
-			postRes.on('end', function () {
-				console.log('CALLBACK - postRes:', response);
-				if (postRes.statusCode != 200) {
-					res.status(postRes.statusCode).json(JSON.parse(response));
+			var getOptions = {
+				url: identity + '?version=latest',
+				method: 'GET',
+				headers: {
+					'Authorization': 'Bearer ' + access_token
+				}
+			};
+			console.log('CALLBACK - GET - options: ' + JSON.stringify(getOptions));
+			request(getOptions, function(error, httpResponse, getResponse) {
+				console.log('CALLBACK - GET - error:', error);
+				console.log('CALLBACK - GET - statusCode:', httpResponse && httpResponse.statusCode);
+				console.log('CALLBACK - GET - headers:', httpResponse && httpResponse.headers);
+				console.log('CALLBACK - GET - response:', getResponse);
+
+				if (httpResponse && httpResponse.statusCode != 200) {
+					res.status(httpResponse.statusCode).json(JSON.parse(getResponse));
 					return;
 				}
 
-				var access_token = response.access_token;
-				var identity = response.id;
+				var html = '<html><head>' +
+						   '<meta name="salesforce-community" content="' + communityUrl + '/embeddedlogin">' +
+						   '<meta name="salesforce-mode" content="modal-callback">' +
+						   '<meta name="salesforce-server-callback" content="true">' +
+						   '<meta name="salesforce-server-response" content="' + new Buffer(getResponse).toString('base64') + '">' +
+						   '<meta name="salesforce-server-starturl" content="' + startURL + '">' +
+						   '<meta name="salesforce-target" content="#salesforce-login">'+
+						   '<meta name="salesforce-allowed-domains" content="https://sfdc-login-token.herokuapp.com">' +
+						   '<script src="' + communityUrl + '/embeddedlogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget&min=false&cacheMaxAge=0" async defer></script>' +
+						   '</head><body></body></html>';
+				console.log('CALLBACK - html: ' + html);
 
-				var getOptions = {
-					url: identity + '?version=latest',
-					method: 'GET',
-					headers: {
-						'Authorization': 'Bearer ' + access_token
-					}
-				};
-				console.log('CALLBACK - GET - getOptions: ' + JSON.stringify(getOptions));
-
-				var getReq = https.request(getOptions, function(getRes) {
-					console.log('CALLBACK - GET - statusCode:', getRes.statusCode);
-					console.log('CALLBACK - GET - headers:', getRes.headers);
-
-					var getResponse = '';
-					getRes.on('data', function (chunk) {
-						getResponse += chunk;
-					});
-
-					getRes.on('end', function () {
-						console.log('CALLBACK - GET - response:', getResponse);
-						if (getRes.statusCode != 200) {
-							res.status(getRes.statusCode).json(JSON.parse(getResponse));
-							return;
-						}
-
-						var html = '<html><head>' +
-								   '<meta name="salesforce-community" content="' + communityUrl + '/embeddedlogin">' +
-								   '<meta name="salesforce-mode" content="modal-callback">' +
-								   '<meta name="salesforce-server-callback" content="true">' +
-								   '<meta name="salesforce-server-response" content="' + new Buffer(getResponse).toString('base64') + '">' +
-								   '<meta name="salesforce-server-starturl" content="' + startURL + '">' +
-								   '<meta name="salesforce-target" content="#salesforce-login">'+
-								   '<meta name="salesforce-allowed-domains" content="https://sfdc-login-token.herokuapp.com">' +
-								   '<script src="' + communityUrl + '/embeddedlogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget&min=false&cacheMaxAge=0" async defer></script>' +
-								   '</head><body></body></html>';
-						console.log('CALLBACK - html: ' + html);
-
-						res.writeHeader(200, {"Content-Type": "text/html"});
-						res.write(html);
-						res.end();
-					});
-				});
-
-				// get
-				getReq.on('error', function(error) {
-					console.error(error);
-				});
-
-				getReq.end();
+				res.writeHeader(200, {"Content-Type": "text/html"});
+				res.write(html);
+				res.end();
 			});
 		});
-
-		// post the data
-		postReq.on('error', function(error) {
-			console.error(error);
-		});
-
-		postReq.write(postData);
-		postReq.end();
 
 	} catch (exception) {
 		console.log(exception);
